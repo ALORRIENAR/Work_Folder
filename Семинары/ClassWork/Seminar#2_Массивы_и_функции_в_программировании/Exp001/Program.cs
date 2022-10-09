9ea9da898f67c7f13e6c98df2924a0787ee7c61c
Rostislav <littamb69@gmail.com>
2022-10-09 17:05:40 +0600

    работа с файлом на уроке

---
+++ Семинары/ClassWork/Seminar#2_Массивы_и_функции_в_программировании/Exp001/Program.cs
@@ -0,0 +1,23 @@
+﻿int FindBiggerDigit(int num)
+
+{
+    int result;
+
+    if(num < 10 || num >=100) 
+        return 0;
+    else
+    {
+        int ed = num  % 10;
+        int dec = num / 10;
+        
+        if (ed > dec)
+            result = ed;
+        else
+            result = dec;
+    }
+
+    return result; 
+}
+
+Console.WriteLine("Введите двухзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
